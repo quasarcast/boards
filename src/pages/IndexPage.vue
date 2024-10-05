0e58fee12bd508032bb8fd7bba1eff3c7a3a6724
Luke Diebold <luke@ldiebold.com>
2024-10-05 20:24:10 +1000

    use newer version of vuemodel

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,24 @@
 <script lang="ts" setup>
+import { useIndexer } from '@vuemodel/core'
+import Project from 'src/models/Project'
 
+const projectsIndexer = useIndexer(Project)
 </script>
 
 <template>
   <q-page padding>
-    Index Page
+    <div class="row q-col-gutter-md">
+      <div
+        v-for="project in projectsIndexer.records.value"
+        :key="project.id"
+        class="col-sm-12 col-md-4 col-lg-3"
+      >
+        <q-card>
+          <q-card-section>
+            {{ project.name }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
